a7ceaa0ff645df81642151a998f30c72a3b897c5
unknown <matthieu.simon1808@gmail.com>
2025-09-29 19:11:24 +0200

    Fix refresh

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,19 +5,23 @@ import SudokuView from '@/pages/SudokuView.vue'
 import ThePriceIsRightView from '@/pages/ThePriceIsRightView.vue'
 import WelcomeView from '@/pages/WelcomeView.vue'
 import WordleView from '@/pages/WordleView.vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [  
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  routes: [
     { path: '/', component: WelcomeView },
     { path: '/the-price-is-right', component: ThePriceIsRightView },
     { path: '/black-and-white', component: BlackAndWhiteView },
     { path: '/wordle', component: WordleView },
     { path: '/reveal-one', component: RevealOneView },
     { path: '/sudoku', component: SudokuView },
-    { path: '/congratulations', component: CongratulationsView }
+    { path: '/congratulations', component: CongratulationsView },
   ],
+  scrollBehavior(to, from, savedPosition) {
+    // always scroll to top
+    return { top: 0 }
+  },
 })
 
 export default router
